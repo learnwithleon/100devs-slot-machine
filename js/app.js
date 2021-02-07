8add42b302ba57451a7a49e9b83313a899e2e60d
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-07 09:23:12 -0300

    working

--- js/app.js
+++ js/app.js
@@ -1,55 +1,22 @@
 console.log("app.js is on")
 
-// class PlayerMaker{
-//     constructor(name){
-//         this.name = name;
-//         this.credit = 100;
-//     }
-//     //methods
-//     storeName(){
-//         localStorage.setItem("playerName", this.name)
-//     }
-//     bet(){
-//         let ammount = document.querySelector("#bet-ammount").value
-//     };
-//     increaseMoney(ammount){
-//         this.credit +=ammount
-//     };
-//     decreaseMoney(ammount){
-//         this.credit -=ammount
-//     };
-// }
-
-// class SlotMachine{
-//     constructor(){
-//         this.possibleValues = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j"]
-//     }
-
-// }
-
-
-
-// document.querySelector("#start-game").addEventListener("click", play)
-
-// function play(){
-//     let name = document.querySelector("#player-name").value
-//     //let name = "Amibo"
-//     let player = new PlayerMaker(name)
-//     console.log(player)
-//     player.storeName()
-//     let slot = new SlotMachine
-// }
-
-let player = {
-    credit : 100
+class PlayerMaker{
+    constructor(){
+        this.name = prompt("what is your name", "Benedict")
+        this.credit = 400
+    }
 }
 
+let player = new PlayerMaker
+
+
 let slotMachine = {
-    possibleValues : [0,1,2,3,4,"a","b","c","d","e"],
+    possibleValues : [0,1,2,3,4,5,6,7,8,9], //,"a","b","c","d","e"
     firstPanel : document.getElementById("first-panel"),
     secondPanel : document.getElementById("second-panel"),
     thirdPanel : document.getElementById("third-panel"),
     roll : document.querySelector("#slot-roll"),
+    resultDisplay: document.querySelector("#result"),
     //methods
     randomPick : function(){
         let random = Math.floor(Math.random()*slotMachine.possibleValues.length)
@@ -60,25 +27,49 @@ let slotMachine = {
 slotMachine.roll.addEventListener("click", play)
 
 function play(){
+    if (player.credit <= 0){
+        alert("Your monies is gonz, boi.")
+        return
+    }
     let betAmmount = Number(document.querySelector("#bet-ammount").value)
-    //console.log(betAmmount)
-    //managePanel()
     let firstPanelPick = slotMachine.randomPick()
     let secondPanelPick = slotMachine.randomPick()
     let thirdPanelPick = slotMachine.randomPick()
-    //console.log(firstPanelPick,secondPanelPick,thirdPanelPick)
+
     slotMachine.firstPanel.innerText = slotMachine.possibleValues[firstPanelPick]
     slotMachine.secondPanel.innerText = slotMachine.possibleValues[secondPanelPick]
     slotMachine.thirdPanel.innerText = slotMachine.possibleValues[thirdPanelPick]
-    if (firstPanelPick === secondPanelPick || secondPanelPick === thirdPanelPick || firstPanelPick === thirdPanelPick){
-        player.credit += betAmmount
-        console.log(player.credit)
-    }else if (firstPanelPick === secondPanelPick && firstPanelPick === thirdPanelPick){
-        player.credit += betAmmount * 3
-        console.log(player.credit)
-    } else {
+    //console.log(firstPanelPick,secondPanelPick,thirdPanelPick)
+    if (firstPanelPick === secondPanelPick && firstPanelPick === thirdPanelPick  && secondPanelPick === thirdPanelPick){
+        let creditAmmount = Number((betAmmount * 5).toFixed(2))
+        player.credit += creditAmmount
+        slotMachine.resultDisplay.innerText = `Jackpot! You won $${creditAmmount}! Current credit is ${player.credit}` 
+        slotMachine.resultDisplay.classList.add("win")
+    }
+    else if(firstPanelPick == secondPanelPick-1 && secondPanelPick == thirdPanelPick-1){
+        let creditAmmount = Number((betAmmount * 1.3).toFixed(2))
+        player.credit += creditAmmount
+        slotMachine.resultDisplay.innerText = `Takeoff bonus! You won $${creditAmmount}! Current credit is ${player.credit}` 
+        slotMachine.resultDisplay.classList.remove("lose")
+        slotMachine.resultDisplay.classList.add("win")
+    }
+    else if(firstPanelPick == secondPanelPick+1 && secondPanelPick == thirdPanelPick+1){
+        let creditAmmount = Number((betAmmount * 1.3).toFixed(2))
+        player.credit += creditAmmount
+        slotMachine.resultDisplay.innerText = `Landing bonus! You won $${creditAmmount}! Current credit is ${player.credit}` 
+        slotMachine.resultDisplay.classList.remove("lose")
+        slotMachine.resultDisplay.classList.add("win")
+    }
+    else if (firstPanelPick === secondPanelPick || secondPanelPick === thirdPanelPick || firstPanelPick === thirdPanelPick){
+        let creditAmmount = Number((betAmmount * 1).toFixed(2))
+        player.credit += creditAmmount
+        slotMachine.resultDisplay.innerText = `You won your $${creditAmmount} back! Current credit is ${player.credit}` 
+        slotMachine.resultDisplay.classList.remove("lose")
+        slotMachine.resultDisplay.classList.add("win")
+    }else {
         player.credit -= betAmmount
-        console.log(player.credit)
+        slotMachine.resultDisplay.innerText = `You lost $${betAmmount}! We are sure you can win on the next round, ${player.name}. Current credit is ${player.credit}` 
+        slotMachine.resultDisplay.classList.add("lose")
     } 
 }
 
